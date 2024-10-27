e6ccb2d3545cfba4893cfcb64ac435c852be2411
hstoykova <hristina.pencheva@gmail.com>
2024-10-27 20:43:46 +0200

    GameTypes seed

---
+++ PokerClubsApp.Data/Configuration/GameTypeConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PokerClubsApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerClubsApp.Data.Configuration
+{
+    public class GameTypeConfiguration : IEntityTypeConfiguration<GameType>
+    {
+        public void Configure(EntityTypeBuilder<GameType> builder)
+        {
+            builder.HasData(this.SeedGameTypes());
+        }
+
+        private List<GameType> SeedGameTypes()
+        {
+            return new List<GameType>()
+            {
+                new GameType { Id = 1, Name = "PLO4"},
+                new GameType { Id = 2, Name = "PLO5"},
+                new GameType { Id = 3, Name = "PLO6"},
+                new GameType { Id = 4, Name = "NLH"}
+            };
+        }
+    }
+}
---
+++ PokerClubsApp.Data/Migrations/20241027184104_SeedGameTypes.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PokerClubsApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedGameTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "GamesTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "PLO4" },
+                    { 2, "PLO5" },
+                    { 3, "PLO6" },
+                    { 4, "NLH" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "GamesTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "GamesTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "GamesTypes",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "GamesTypes",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
