a286853ef52469eb501201916c90340eb9c4fcc8
hstoykova <hristina.pencheva@gmail.com>
2024-11-05 20:39:12 +0200

    Added GameTypes select input

--- PokerClubsApp/Controllers/GameResultsController.cs
+++ PokerClubsApp/Controllers/GameResultsController.cs
@@ -1,14 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PokerClubsApp.Data;
+using PokerClubsApp.Data.Models;
+using PokerClubsApp.Web.ViewModels.Game;
 
 namespace PokerClubsApp.Controllers
 {
     public class GameResultsController : Controller
     {
+        private readonly PokerClubsDbContext context;
+
+        public GameResultsController(PokerClubsDbContext _context)
+        {
+            context = _context;
+        }
+
         [HttpGet]
         public async Task<IActionResult> Add()
         {
+            var model = new AddGameResultsModel()
+            {
+                GameTypes = await GetAllGameTypes()
+            };
+            //model.GameTypes = await GetAllGameTypes();
 
-            return this.View();
+            return this.View(model);
+        }
+
+        private async Task<List<GameType>> GetAllGameTypes()
+        {
+            return await context.GamesTypes.ToListAsync();
         }
     }
 }
