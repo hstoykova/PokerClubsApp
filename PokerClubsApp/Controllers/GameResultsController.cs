032305f4c9f9d12d08c495a3cfcef292707383a2
hstoykova <hristina.pencheva@gmail.com>
2024-11-05 20:19:44 +0200

    GameResultsController skeletton created

---
+++ PokerClubsApp/Controllers/GameResultsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PokerClubsApp.Controllers
+{
+    public class GameResultsController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+
+            return this.View();
+        }
+    }
+}
