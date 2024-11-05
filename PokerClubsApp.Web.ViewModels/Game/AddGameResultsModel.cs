7a1b8a463f8358a781e5fb14c5110b69e97e5f3a
hstoykova <hristina.pencheva@gmail.com>
2024-11-05 20:17:47 +0200

    AddGameResults ViewModel added

---
+++ PokerClubsApp.Web.ViewModels/Game/AddGameResultsModel.cs
@@ -0,0 +1,39 @@
+﻿using PokerClubsApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PokerClubsApp.Common.EntityValidationConstants.Player;
+
+namespace PokerClubsApp.Web.ViewModels.Game
+{
+    public class AddGameResultsModel
+    {
+        [Required]
+        public int AccountId { get; set; }
+
+        [Required]
+        [MinLength(PlayerNicknameMinLength)]
+        [MaxLength(PlayerNicknameMaxLength)]
+        public string Nickname { get; set; } = null!;
+
+        [Required]
+        public int ClubId { get; set; }
+
+        [Required]
+        public decimal Result { get; set; }
+
+        [Required]
+        public decimal Fee { get; set; }
+
+        [Required]
+        public DateTime EndedAt { get; set; }
+
+        [Required]
+        public int GameTypeId { get; set; }
+
+        public List<GameType> GameTypes { get; set; } = new();
+    }
+}
