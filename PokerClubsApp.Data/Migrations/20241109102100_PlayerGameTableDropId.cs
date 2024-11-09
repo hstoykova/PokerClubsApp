0c00e21573781a30de92d9ea2233b333d8563cec
hstoykova <hristina.pencheva@gmail.com>
2024-11-09 12:24:30 +0200

    PlayerGame table drop Id

---
+++ PokerClubsApp.Data/Migrations/20241109102100_PlayerGameTableDropId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PokerClubsApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PlayerGameTableDropId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "PlayersGames");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "PlayersGames",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
