1b717edca553f72af6f54282aee8de08753f9ecc
hstoykova <hristina.pencheva@gmail.com>
2024-11-19 22:28:33 +0200

    Union controller added

---
+++ PokerClubsApp.Web.ViewModels/Unions/AddUnionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static PokerClubsApp.Common.EntityValidationConstants.Union;
+
+namespace PokerClubsApp.Web.ViewModels.Unions
+{
+    public class AddUnionModel
+    {
+        [Required]
+        [MinLength(UnionNameMinLength)]
+        [MaxLength(UnionNameMaxLength)]
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ PokerClubsApp/Controllers/UnionsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using PokerClubsApp.Data;
+using PokerClubsApp.Data.Models;
+using PokerClubsApp.Web.ViewModels.Game;
+using PokerClubsApp.Web.ViewModels.Unions;
+
+namespace PokerClubsApp.Controllers
+{
+    public class UnionsController : Controller
+    {
+        private readonly PokerClubsDbContext context;
+
+        public UnionsController(PokerClubsDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var model = new AddUnionModel();
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AddUnionModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            Union union = new Union() { Name = model.Name };
+            await context.Unions.AddAsync(union);
+            await context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
