b9d4bbb36136f05f4fd1b31445e827e47aee3cb1
hstoykova <hristina.pencheva@gmail.com>
2024-11-21 22:17:39 +0200

    Clubs controller added; method Create

---
+++ PokerClubsApp.Web.ViewModels/Clubs/AddClubModel.cs
@@ -0,0 +1,19 @@
+﻿using PokerClubsApp.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using static PokerClubsApp.Common.EntityValidationConstants.Club;
+
+namespace PokerClubsApp.Web.ViewModels.Clubs
+{
+    public class AddClubModel
+    {
+        [Required]
+        [MinLength(ClubNameMinLength)]
+        [MaxLength(ClubNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        public int UnionId { get; set; }
+
+        public List<Union> Unions { get; set; } = new();
+    }
+}
---
+++ PokerClubsApp/Controllers/ClubsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PokerClubsApp.Data;
+using PokerClubsApp.Data.Models;
+using PokerClubsApp.Web.ViewModels.Clubs;
+using PokerClubsApp.Web.ViewModels.Unions;
+
+namespace PokerClubsApp.Controllers
+{
+    public class ClubsController : Controller
+    {
+        private readonly PokerClubsDbContext context;
+
+        public ClubsController(PokerClubsDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateAsync()
+        {
+            var model = new AddClubModel();
+            model.Unions = await context.Unions.ToListAsync();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AddClubModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Unions = await context.Unions.ToListAsync();
+                return View(model);
+            }
+
+            Club club = new Club() 
+            { 
+                Name = model.Name,
+                UnionId = model.UnionId
+            };
+            
+            await context.Clubs.AddAsync(club);
+            await context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
