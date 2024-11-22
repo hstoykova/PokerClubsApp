94410a9ea68e4ffe08df459020a166b7b0e608f7
hstoykova <hristina.pencheva@gmail.com>
2024-11-22 20:58:18 +0200

    Edit method added to Unions controller

--- PokerClubsApp/Controllers/UnionsController.cs
+++ PokerClubsApp/Controllers/UnionsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PokerClubsApp.Data;
 using PokerClubsApp.Data.Models;
 using PokerClubsApp.Web.ViewModels.Unions;
@@ -32,6 +33,47 @@ namespace PokerClubsApp.Controllers
 
             Union union = new Union() { Name = model.Name };
             await context.Unions.AddAsync(union);
+            await context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = await context.Unions
+                .Where(u => u.Id == id)
+                .Where(u => u.IsDeleted == false)
+                .AsNoTracking()
+                .Select(u => new AddUnionModel()
+                {
+                    Name = u.Name
+                })
+                .FirstOrDefaultAsync();
+
+            if(model is null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(AddUnionModel model, int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var union = await context.Unions
+                .Where(u => u.Id == id)
+                .Where(u => u.IsDeleted == false)
+                .FirstOrDefaultAsync();
+
+            union!.Name = model.Name;
+
             await context.SaveChangesAsync();
 
             return RedirectToAction("Index", "Home");
