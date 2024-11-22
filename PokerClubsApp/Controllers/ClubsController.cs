d9d860847eb57cab49ec14988fd5fd66c6b132c7
hstoykova <hristina.pencheva@gmail.com>
2024-11-22 15:58:30 +0200

    Edit method added to Club controller

--- PokerClubsApp/Controllers/ClubsController.cs
+++ PokerClubsApp/Controllers/ClubsController.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using PokerClubsApp.Data;
 using PokerClubsApp.Data.Models;
 using PokerClubsApp.Web.ViewModels.Clubs;
-using PokerClubsApp.Web.ViewModels.Unions;
 
 namespace PokerClubsApp.Controllers
 {
@@ -17,7 +16,7 @@ namespace PokerClubsApp.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> CreateAsync()
+        public async Task<IActionResult> Create()
         {
             var model = new AddClubModel();
             model.Unions = await context.Unions.ToListAsync();
@@ -45,5 +44,70 @@ namespace PokerClubsApp.Controllers
 
             return RedirectToAction("Index", "Home");
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var model = await context.Clubs
+                .Where(c => c.IsDeleted == false)
+                .Where(c => c.Id == id)
+                .Select(c => new AddClubModel()
+                {
+                    Name = c.Name,
+                    UnionId = c.UnionId
+                })
+                .FirstOrDefaultAsync();
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            model.Unions = await context.Unions.ToListAsync();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(AddClubModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Unions = await context.Unions.ToListAsync();
+                return View(model);
+            }
+
+            Club club = new Club()
+            {
+                Name = model.Name,
+                UnionId = model.UnionId
+            };
+
+            await context.Clubs.AddAsync(club);
+            await context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        //[HttpGet]
+        //public async Task<IActionResult> Index(AddClubModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var clubs = await context.Clubs
+        //        .Where(c => c.IsDeleted == false)
+        //        .AsNoTracking()
+        //        .ToListAsync();
+
+        //    var unions = await context.Unions
+        //        .Where(u => u.IsDeleted == false)
+        //        .AsNoTracking()
+        //        .ToListAsync();
+
+        //    return View(model);
+        //}
     }
 }
